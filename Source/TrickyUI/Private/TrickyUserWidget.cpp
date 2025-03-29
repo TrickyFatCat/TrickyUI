23051d554788d8a5e13a446f9e4d86334c61219c
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-03-29 14:18:04 +0200

    Create TrickyUserWidget class

---
+++ Source/TrickyUI/Private/TrickyUserWidget.cpp
@@ -0,0 +1,67 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+
+#include "TrickyUserWidget.h"
+
+void UTrickyUserWidget::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+}
+
+void UTrickyUserWidget::Show()
+{
+	const float StartTime = CalculateAnimationStartTime(HideAnimation, ShowAnimation);
+	PlayAnimation(ShowAnimation, StartTime);
+}
+
+void UTrickyUserWidget::Hide()
+{
+	const float StartTime = CalculateAnimationStartTime(ShowAnimation, HideAnimation);
+	PlayAnimation(HideAnimation, StartTime);
+}
+
+void UTrickyUserWidget::OnAnimationStarted_Implementation(const UWidgetAnimation* Animation)
+{
+	SetVisibility(ESlateVisibility::HitTestInvisible);
+	
+	if (Animation == ShowAnimation)
+	{
+		OnShowAnimationStarted.Broadcast();
+		HandleShowAnimationStarted();
+	}
+	else if (Animation == HideAnimation)
+	{
+		OnHideAnimationStarted.Broadcast();
+		HandleHideAnimationStarted();
+	}
+}
+
+void UTrickyUserWidget::OnAnimationFinished_Implementation(const UWidgetAnimation* Animation)
+{
+	if (Animation == ShowAnimation)
+	{
+		SetVisibility(OnShowFinishedVisibilityState);
+		OnShowAnimationFinished.Broadcast();
+		HandleShowAnimationFinished();
+	}
+	else if (Animation == HideAnimation)
+	{
+		SetVisibility(OnHideFinishedVisibilityState);
+		OnHideAnimationFinished.Broadcast();
+		HandleHideAnimationFinished();
+	}
+}
+
+float UTrickyUserWidget::CalculateAnimationStartTime(const UWidgetAnimation* Animation,
+	const UWidgetAnimation* NewAnimation) const
+{
+	if (!Animation || !NewAnimation)
+	{
+		return -1.f;
+	}
+
+	const float AnimationEndTime = Animation->GetEndTime();
+	const float NewAnimationEndTime = NewAnimation->GetEndTime();
+	const float TargetTime = ((AnimationEndTime - GetAnimationCurrentTime(Animation)) / AnimationEndTime) * NewAnimationEndTime;
+	return TargetTime * static_cast<float>(IsAnimationPlaying(Animation));	
+}
---
+++ Source/TrickyUI/Public/TrickyUserWidget.h
@@ -0,0 +1,74 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/WidgetAnimation.h"
+#include "Blueprint/UserWidget.h"
+#include "TrickyUserWidget.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnAnimationStartedDynamicSignature);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnAnimationFinishedDynamicSignature);
+
+/**
+ * 
+ */
+UCLASS(Abstract)
+class TRICKYUI_API UTrickyUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeOnInitialized() override;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnAnimationStartedDynamicSignature OnShowAnimationStarted;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnAnimationFinishedDynamicSignature OnShowAnimationFinished;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnAnimationStartedDynamicSignature OnHideAnimationStarted;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnAnimationFinishedDynamicSignature OnHideAnimationFinished;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "TrickyUserWidget")
+	ESlateVisibility OnShowFinishedVisibilityState  = ESlateVisibility::Visible;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "TrickyUserWidget")
+	ESlateVisibility OnHideFinishedVisibilityState  = ESlateVisibility::Hidden;
+
+	UFUNCTION(BlueprintCallable, Category = "TrickyUserWidget")
+	void Show();
+
+	UFUNCTION(BlueprintCallable, Category = "TrickyUserWidget")
+	void Hide();
+
+
+protected:
+	UPROPERTY(Transient, meta=(BindWidgetAnim))
+	TObjectPtr<UWidgetAnimation> ShowAnimation = nullptr;
+	
+	UPROPERTY(Transient, meta=(BindWidgetAnim))
+	TObjectPtr<UWidgetAnimation> HideAnimation = nullptr;
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "TrickyUserWidget")
+	void HandleShowAnimationStarted();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "TrickyUserWidget")
+	void HandleShowAnimationFinished();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "TrickyUserWidget")
+	void HandleHideAnimationStarted();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "TrickyUserWidget")
+	void HandleHideAnimationFinished();
+
+	virtual void OnAnimationStarted_Implementation(const UWidgetAnimation* Animation) override;
+
+	virtual void OnAnimationFinished_Implementation(const UWidgetAnimation* Animation) override;
+
+	float CalculateAnimationStartTime(const UWidgetAnimation* Animation, const UWidgetAnimation* NewAnimation) const;
+};
