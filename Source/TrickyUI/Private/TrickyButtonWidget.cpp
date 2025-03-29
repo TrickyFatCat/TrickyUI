15adacd1021353050762b9361118768af7362bd9
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-03-29 14:32:13 +0200

    Create TrickyButtonWidget class

---
+++ Source/TrickyUI/Private/TrickyButtonWidget.cpp
@@ -0,0 +1,45 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+
+#include "TrickyButtonWidget.h"
+
+#include "Components/Button.h"
+
+void UTrickyButtonWidget::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	if (IsValid(Button_Main))
+	{
+		Button_Main->OnClicked.AddUniqueDynamic(this, &UTrickyButtonWidget::HandleButtenClicked);
+		Button_Main->OnPressed.AddUniqueDynamic(this, &UTrickyButtonWidget::HandleButtonPressed);
+		Button_Main->OnReleased.AddUniqueDynamic(this, &UTrickyButtonWidget::HandleButtonReleased);
+		Button_Main->OnHovered.AddUniqueDynamic(this, &UTrickyButtonWidget::HandleButtonHovered);
+		Button_Main->OnUnhovered.AddUniqueDynamic(this, &UTrickyButtonWidget::HandleButtonUnhovered);
+	}
+}
+
+void UTrickyButtonWidget::HandleButtenClicked()
+{
+	OnButtonClicked.Broadcast(this);
+}
+
+void UTrickyButtonWidget::HandleButtonPressed()
+{
+	OnButtonPressed.Broadcast(this);
+}
+
+void UTrickyButtonWidget::HandleButtonReleased()
+{
+	OnButtonReleased.Broadcast(this);
+}
+
+void UTrickyButtonWidget::HandleButtonHovered()
+{
+	OnButtonHovered.Broadcast(this);
+}
+
+void UTrickyButtonWidget::HandleButtonUnhovered()
+{
+	OnButtonUnhovered.Broadcast(this);
+}
---
+++ Source/TrickyUI/Public/TrickyButtonWidget.h
@@ -0,0 +1,66 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TrickyUserWidget.h"
+#include "TrickyButtonWidget.generated.h"
+
+class UButton;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnButtonClickedDynamicSignature, UTrickyButtonWidget*, Button);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnButtonPressedDynamicSignature, UTrickyButtonWidget*, Button);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnButtonReleasedDynamicSignature, UTrickyButtonWidget*, Button);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnButtonHoveredDynamicSignature, UTrickyButtonWidget*, Button);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnButtonUnhoveredDynamicSignature, UTrickyButtonWidget*, Button);
+
+/**
+ * 
+ */
+UCLASS()
+class TRICKYUI_API UTrickyButtonWidget : public UTrickyUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeOnInitialized() override;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnButtonClickedDynamicSignature OnButtonClicked;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnButtonPressedDynamicSignature OnButtonPressed;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnButtonReleasedDynamicSignature OnButtonReleased;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnButtonHoveredDynamicSignature OnButtonHovered;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnButtonUnhoveredDynamicSignature OnButtonUnhovered;
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category="ButtonWidget", meta=(BindWidget))
+	TObjectPtr<UButton> Button_Main = nullptr;
+
+private:
+	UFUNCTION()
+	void HandleButtenClicked();
+
+	UFUNCTION()
+	void HandleButtonPressed();
+
+	UFUNCTION()
+	void HandleButtonReleased();
+
+	UFUNCTION()
+	void HandleButtonHovered();
+
+	UFUNCTION()
+	void HandleButtonUnhovered();
+};
